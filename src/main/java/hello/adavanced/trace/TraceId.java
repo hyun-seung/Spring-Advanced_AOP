551f318565446b06f34ea14334c4eba7e542a807
hyunseung <ardimento22@naver.com>
2024-10-13 02:49:56 +0900

    init Project

---
+++ src/main/java/hello/adavanced/trace/TraceId.java
@@ -0,0 +1,23 @@
+package hello.adavanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+}
---
+++ src/test/java/hello/adavanced/AdavancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.adavanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdavancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
