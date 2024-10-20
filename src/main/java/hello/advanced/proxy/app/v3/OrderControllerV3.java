f45a778559dd24efba0e67e84c4f2f5afae53b98
hyunseung <ardimento22@naver.com>
2024-10-20 23:42:10 +0900

    자동 빈 등록

--- src/main/java/hello/advanced/proxy/app/v3/OrderControllerV3.java
+++ src/main/java/hello/advanced/proxy/app/v3/OrderControllerV3.java
@@ -1,31 +1,31 @@
-package hello.advanced.proxy.app.v2;
+package hello.advanced.proxy.app.v3;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 //@RequestMapping //Spring 6.x부터 @RequestMapping는 Controller로 등록 불가
-@Controller
+@RestController
 @ResponseBody
-public class OrderControllerV2 {
+public class OrderControllerV3 {
 
-    private final OrderServiceV2 orderService;
+    private final OrderServiceV3 orderService;
 
-    public OrderControllerV2(OrderServiceV2 orderService) {
+    public OrderControllerV3(OrderServiceV3 orderService) {
         this.orderService = orderService;
     }
 
-    @GetMapping("/v2/request")
+    @GetMapping("/v3/request")
     public String request(@RequestParam("itemId") String itemId) {
         orderService.orderItem(itemId);
         return "ok";
     }
 
-    @GetMapping("/v2/no-log")
+    @GetMapping("/v3/no-log")
     public String noLog() {
         return "ok";
     }
