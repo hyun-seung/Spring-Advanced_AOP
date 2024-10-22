141237cc89d6c0cbaadd530c02cec4877321c212
hyunseung <ardimento22@naver.com>
2024-10-22 23:54:26 +0900

    인터페이스 기반 프록시 적용

--- src/main/java/hello/advanced/AdavancedApplication.java
+++ src/main/java/hello/advanced/AdavancedApplication.java
@@ -2,16 +2,25 @@ package hello.advanced;
 
 import hello.advanced.proxy.config.AppV1Config;
 import hello.advanced.proxy.config.AppV2Config;
+import hello.advanced.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.advanced.trace.logTrace.LogTrace;
+import hello.advanced.trace.logTrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class})
-@SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app") // 주의
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import(InterfaceProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.advanced.proxy.app.v1") // 주의
 public class AdavancedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdavancedApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
+	}
 }
---
+++ src/main/java/hello/advanced/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -0,0 +1,28 @@
+package hello.advanced.proxy.config.v1_proxy.interface_proxy;
+
+import hello.advanced.proxy.app.v1.OrderRepositoryV1;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logTrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+
+    private final OrderRepositoryV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.request()");
+            //targe 호출
+            target.save(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
